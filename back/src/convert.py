12c66c85f408e2d4ba273943140064bdc885f6b7
Aborgen <alexanderborgen@gmail.com>
2019-12-12 15:53:42 -0700

    Create convert.py, which will house functions that change numbers in some fashion

---
+++ back/src/convert.py
@@ -0,0 +1,8 @@
+from math import log10
+import numpy as np
+
+def dBFStoGainAmps(dBs: np.ndarray) -> np.ndarray:
+  return np.round(10 ** (dBs / 20), 2)
+
+def gainAmpTodBFS(amps: np.ndarray) -> np.ndarray:
+  return 20 * np.log10(amps)
