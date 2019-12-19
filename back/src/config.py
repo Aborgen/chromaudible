5bfbf1025a50753a55c7fe5500c0cc24c6d4797b
Aborgen <alexanderborgen@gmail.com>
2019-12-19 16:32:25 -0700

    Fix wrong numbers

--- back/src/config.py
+++ back/src/config.py
@@ -5,18 +5,20 @@ from shutil import rmtree
 tempDir = Path("../temp")
 sampleRate = 44100
 melodyBounds = {
-  'minBound': 55.0, 'maxBound': 800.0,
-  'newMin'  : 0,    'newMax'  : 360
+  'minBound': 55.0, 'maxBound': 1760.0,
+  'newMin'  : 0,    'newMax'  : 360.0
 }
 
 timbreBounds = {
   'minBound': 350.0, 'maxBound': 5000.0,
-  'newMin'  : 0,     'newMax'  : 100
+  'newMin'  : 0,     'newMax'  : 100.0
 }
 
+# The range of loudness is [-1, +1], due to the fact that the values
+# are dBFS converted into ampheres utilizing convert.dBFStoGainAmps
 loudnessBounds = {
-  'minBound': -100.0, 'maxBound': 0,
-  'newMin'  : 0,      'newMax'  : 100
+  'minBound': -1.0,     'maxBound': 1.0,
+  'newMin'  : 0,        'newMax'  : 100.0
 }
 
 def _init():
