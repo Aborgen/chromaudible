4ad9eefdbd0918a25ab40d457615f670587dbf08
Aborgen <alexanderborgen@gmail.com>
2019-12-24 17:06:16 -0700

    Add functions that convert from melodyParts to a list of html hex string colors
    The general path is:
    1) Isolate vocals
    2) Extract melody from melodia, normalized between 0 and 1
    3) Extract 'volume changes' present in vocals, normalized between 0 and 1
    4) Extract timbre, normalized between 0 and 1
    5) Convert from hls to rgb colorspaces
      * h -> melody at time
      * l -> constant timbre value
      * s -> 'volume change' at time
    6) Finally, Convert from rgb to html hex string colors

--- back/src/convert.py
+++ back/src/convert.py
@@ -1,6 +1,9 @@
+from config import tempDir
+from colorsys import hls_to_rgb
 from math import log10
 import numpy as np
-from typing import List, Union
+from operator import itemgetter
+from typing import Dict, List, Tuple, Union
 
 def dBFStoGainAmps(dBs: np.ndarray) -> np.ndarray:
   return np.round(10 ** (dBs / 20), 2)
@@ -25,3 +28,48 @@ def clampOne(n: Union[int, float], minBound: Union[int, float], maxBound: Union[
 
 def clampAll(arr: np.ndarray, minBound: Union[int, float], maxBound: Union[int, float]) -> np.ndarray:
   return np.clip(arr, minBound, maxBound)
+ 
+def quantizeRgb(rgb: Tuple[float]) -> Tuple[int]:
+  intRgb = []
+  for n in rgb:
+    assert 0 <= n <= 1.0
+    n = int(clampOne(n, minBound=0, maxBound=1.0) * 256.0)
+    if n == 256:
+      n = 255
+
+    intRgb.append(n)
+
+  return tuple(intRgb)
+
+def melodyPartsToHexColor(melodyParts: Dict) -> Dict[int, str]:
+  melody, volumeChanges, timbreTexture = itemgetter('melody', 'volumeChanges', 'timbreTexture')(melodyParts)
+  h = 0.0
+  l = volumeChanges[sorted(volumeChanges.keys())[0]]
+  s = timbreTexture
+  colorTimeMap = dict()
+  for i, (t, freq) in enumerate(melody):
+    if i == 0:
+      print(h, l, s)
+      print(hls_to_rgb(h, l, s))
+      print(rgbToHex(hls_to_rgb(h, l, s)))
+
+    h = freq
+    if t in volumeChanges:
+      l = volumeChanges[t]
+
+    colorTimeMap[t] = rgbToHex(hls_to_rgb(h, l, s))
+  
+  return colorTimeMap
+
+def rgbToHex(rgb: Tuple[float]) -> str:
+  r, g, b = quantizeRgb(rgb)
+  return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+# Based off of terrygarcia's stackexchange answer:
+# https://stackoverflow.com/a/57777266
+def hexToRgb(hexString: str) -> Tuple[int]:
+  n = int(hexString[1:], 16)
+  b = n % 256.0
+  g = (n - b) / 256.0 % 256.0
+  r = (n - b) / 256.0 ** 2 - g / 256.0
+  return (int(r), int(g), int(b))
