f4a19b878f52218305b747185e092dac5d89ef3d
Aborgen <alexanderborgen@gmail.com>
2020-01-10 16:27:44 -0700

    Complete MusicPlayer

--- front/src/utils/MusicPlayer.js
+++ front/src/utils/MusicPlayer.js
@@ -1,48 +1,97 @@
-
-
 class MusicPlayer {
   constructor(context, frequencyTimePairs) {
     this.context = context;
     this.frequencyTimePairs = frequencyTimePairs;
-    this.noteLength = 50/*ms*/;
-    this.scheduleAhead = 100/*ms*/;
-    this.cooldownPeriod = 20/*ms*/;
+    this.lastTimePair = [];
+    this.scheduleAhead = 0.1/*s*/;
+    this.cooldownPeriod = 25/*ms*/;
     this.isFinished = false;
+    this.notePtr = 0;
+    [this.instrument, this.gain] = this.initInstrument();
   }
 
-  scheduleNote(hz, timestamp, length) {
+  initInstrument() {
+    // Initialize oscillator
     const osc = this.context.createOscillator();
-    osc.frequency.value = hz;
-    osc.start(timestamp);
-    osc.stop(timestamp + length);
+    osc.frequency.setValueAtTime(0, this.context.currentTime);
+    osc.connect(this.context.destination);
+    // Initialize oscillator gain
+    const gain = this.context.createGain();
+    gain.gain.setValueAtTime(0.3, this.context.currentTime);
+    osc.connect(gain);
+    gain.connect(this.context.destination);
+    return [osc, gain];
+  }
+  
+  instrumentOn() {
+    this.instrument.start(this.context.currentTime);
   }
 
+  instrumentOff() {
+    this.instrument.stop(this.context.currentTime);
+    this.instrument = this.initInstrument();
+  }
+
+  setGain(n, timestamp) {
+    if (n < -1.0 || n > 1.0) {
+      return;
+    }
+
+    this.gain.gain.setTargetAtTime(n, timestamp, 0.015);
+  }
+
+  tuneInstrument(hz, timestamp) {
+    if (this.instrument.frequency.value == hz) {
+      return;
+    }
+
+    this.instrument.frequency.setValueAtTime(hz, timestamp);
+  }
+  
+  nextNote() {
+    if (this.notePtr >= this.frequencyTimePairs.length - 1) {
+      this.isFinished = true;
+      this.instrumentOff();
+      return [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY];
+    }
+
+    return this.frequencyTimePairs[this.notePtr++];
+  }
+
+  // Based on Chris Wilson's metronome scheduler:
+  // https://www.html5rocks.com/en/tutorials/audio/scheduling/
   scheduler() {
     if (this.isFinished) {
       return;
     }
 
-    let [hz, timestamp] = this.nextNote();
+    let timestamp, hz;
+    if (this.lastTimePair.length != 0) {
+      [timestamp, hz] = this.lastTimePair;
+      this.lastTimePair = [];
+    }
+    else {
+      [timestamp, hz] = this.nextNote();
+    }
+
     while (timestamp < this.context.currentTime + this.scheduleAhead) {
-      this.scheduleNote(hz, timestamp, this.noteLength);
-      [hz, timestamp] = this.nextNote();
+      this.tuneInstrument(hz, timestamp);
+      [timestamp, hz] = this.nextNote();
+    }
+    // Context is suspended at construction, freezing currentTime at 0,
+    // and enabling scheduling of notes at 0 seconds.
+    if (this.context.state == 'suspended') {
+      this.context.resume();
     }
 
-    window.setTimeout(this.scheduler, this.cooldownPeriod);
-  }
-
-  nextNote() {
-    if (this.frequencyTimePairs.length == 0) {
-      this.isFinished = true;
-      return [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY];
-    }
-
-    return this.frequencyTimePairs.shift();
+    this.lastTimePair = [timestamp, hz];
+    window.setTimeout(this.scheduler.bind(this), this.cooldownPeriod);
   }
 
   play() {
+    this.instrumentOn();
     this.scheduler();
-    return 'Done playing!';
+    return;
   }
 }
 
