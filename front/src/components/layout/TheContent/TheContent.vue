483389c2eddd3de37dd7b49ef0fe548e983eb144
Aborgen <alexanderborgen@gmail.com>
2020-01-14 15:29:52 -0700

    Add logic to conditionally display AudioUpload or the soon-to-exist ImageUpload components

--- front/src/components/layout/TheContent/TheContent.vue
+++ front/src/components/layout/TheContent/TheContent.vue
@@ -1,33 +1,46 @@
 <template>
 <section :class="classFromParent + ' content-container'">
   <header>Time to do the thing</header>
-  <section>
-    <file-form
-      :input-label='currentLabel'
-      :action='currentAction'
-      form-name='upload'
-    />
-  </section>
+  <button @click='setAudioMode'>Upload Audio</button>
+  <button @click='setImageMode'>Upload Image</button>
+  <audio-upload v-if="currentMode === ModeEnum.AUDIO" />
+  <div v-else-if="currentMode === ModeEnum.IMAGE">WATCH THIS SPACE</div>
 </section>
-
 </template>
 
 <script>
-import FileForm from '../../FileForm/FileForm';
+import AudioUpload from './internal/AudioUpload/AudioUpload';
+import ModeEnum from './internal/misc/ModeEnum';
+
+function setAudioMode() {
+  if (this.currentMode !== ModeEnum.AUDIO) {
+    this.currentMode = ModeEnum.AUDIO;
+  }
+}
+
+function setImageMode() {
+  if (this.currentMode !== ModeEnum.IMAGE) {
+    this.currentMode = ModeEnum.IMAGE;
+  }
+}
 
 export default {
   name: 'TheContent',
   components: {
-    FileForm
+    AudioUpload
   },
-  data() {
+  data: function() {
     return {
-      currentLabel: "Upload your music!",
-      currentAction: '#'
+      currentMode: ModeEnum.AUDIO,
+      ModeEnum
     }
   },
+  methods: {
+    setAudioMode,
+    setImageMode
+  },
   props: {
-    classFromParent: {
+    'classFromParent': {
       type: String,
       required: true
     }
