a47ef28cc6312c369e277aae7c55a7071374bc2a
Aborgen <alexanderborgen@gmail.com>
2020-03-17 16:00:09 -0700

    Update structure of FileForm HTML, as well as add styling

--- front/src/components/FileForm/FileForm.vue
+++ front/src/components/FileForm/FileForm.vue
@@ -1,8 +1,13 @@
 <template>
-  <form @submit.prevent='handleSubmit' :class="formName + '_form'">
-    <label for="upload">{{ inputLabel }}</label>
-    <input id="upload" name="upload" type="file" :class="formName + '_input'" :disabled='formDisabled'>
-    <input type="submit" value="submit" :disabled='formDisabled'>
+  <form @submit.prevent='handleSubmit' :class="formName + '_form file-form'">
+    <section class='upload-group file-form-item'>
+      <label for="upload">{{ inputLabel }}</label>
+      <input id="upload" name="upload" type="file"
+        :class="formName + '_input'" :disabled='formDisabled'
+        :accept='acceptedTypes'
+      >
+    </section>
+    <input class='file-form-item' type="submit" value="submit" :disabled='formDisabled'>
   </form>
 </template>
 
@@ -25,7 +30,29 @@ export default {
     inputLabel: {
       type: String,
       required: true
+    },
+    acceptedTypes: {
+      type: String,
+      required: true
     }
   }
 }
 </script>
+
+
+<style scoped>
+  .file-form {
+    display: flex;
+    justify-content: space-between;
+    align-items: stretch;
+    flex-flow: row wrap;
+    font-size: 2rem;
+  }
+
+  .upload-group {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+    text-align: left;
+  }
+</style>
