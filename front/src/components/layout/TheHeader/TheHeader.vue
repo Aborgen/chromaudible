307c4e35946558908e108f353139f7b78d392ff0
Aborgen <alexanderborgen@gmail.com>
2020-03-17 16:39:18 -0700

    Add styling to TheHeader

--- front/src/components/layout/TheHeader/TheHeader.vue
+++ front/src/components/layout/TheHeader/TheHeader.vue
@@ -23,12 +23,25 @@ export default {
 
 <style scoped>
 .header-container {
-
+  display: flex;
+  background: #b0cadf;
+  flex: 0 1 15%;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
 }
 
 .header-item {
-  background: orange;
-  padding: 10px;
+  flex: 0 0 20%;
+  padding: 2rem;
+}
+
+.logo {
+  border: 0.25rem solid black;
+  border-top: none;
+  border-bottom: none;
+  letter-spacing: 0.25rem;
+  text-shadow: 4px 2px #af7000;
 }
 
 </style>
