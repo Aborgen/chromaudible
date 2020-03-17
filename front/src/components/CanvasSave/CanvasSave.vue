34c6846a9eb97c091e130474f562995bb85417eb
Aborgen <alexanderborgen@gmail.com>
2020-03-17 15:58:32 -0700

    Update id and class names to be kebab cased, rather than camel cased

--- front/src/components/CanvasSave/CanvasSave.vue
+++ front/src/components/CanvasSave/CanvasSave.vue
@@ -1,27 +1,27 @@
 <template>
-<section class='canvasDownload'>
-  <canvas id='canvasView' :width='width' :height='height'></canvas>
-  <canvas id='realCanvas'></canvas>
-  <a id='canvasAnchor' :download="downloadProps.name + '.png'">{{ downloadProps.text }}</a>
+<section class='canvas-download'>
+  <canvas id='canvas-view' :width='width' :height='height'></canvas>
+  <canvas id='real-canvas'></canvas>
+  <a id='canvas-anchor' :download="downloadProps.name + '.png'">{{ downloadProps.text }}</a>
 </section>
 </template>
 <script>
 
 function drawCanvas(drawFunction) {
-  const canvas = document.getElementById('realCanvas');
+  const canvas = document.getElementById('real-canvas');
   drawFunction(canvas);
   generateThumbnail(canvas);
   linkCanvas(canvas);
 }
 
 function generateThumbnail(canvas) {
-  const thumbnail = document.getElementById('canvasView');
+  const thumbnail = document.getElementById('canvas-view');
   thumbnail.getContext('2d').drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, thumbnail.width, thumbnail.height);
 }
 
 function linkCanvas(canvas) {
   const data = canvas.toDataURL('image/png');
-  const a = document.getElementById('canvasAnchor');
+  const a = document.getElementById('canvas-anchor');
   a.href = data;
 }
 
@@ -52,17 +52,25 @@ export default {
 </script>
 
 <style scoped>
-  #canvasView {
+  #canvas-view {
     border: 2px solid #000000;
     display: block;
   }
 
-  #realCanvas {
+  #real-canvas {
     border: 2px solid #000000;
     display: none;
   }
 
-  #canvasAnchor {
+  #canvas-anchor {
     display: block;
+    font-size: 3rem;
+  }
+
+  .canvas-download {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: stretch;
+    align-items: center;
   }
 </style>
