075aa695fe41994713bda94f8c0839aafa46065f
Aborgen <alexanderborgen@gmail.com>
2020-03-17 16:13:00 -0700

    Update convertToImage to ensure that input is received, and alert the user if the server is unreachable

--- front/src/components/layout/TheContent/internal/AudioUpload/AudioUpload.vue
+++ front/src/components/layout/TheContent/internal/AudioUpload/AudioUpload.vue
@@ -24,13 +24,51 @@ import fetchCatch from 'utils/FetchCatch';
 import FileForm from 'components/FileForm/FileForm';
 import { spiral, calculateSpiralPeak } from 'utils/Spiral';
 
+function resetState(stopTrying, parentMessage) {
+  if (stopTrying) {
+    this.currentLabel = parentMessage;
+    this.formDisabled = true;
+    return;
+  }
+
+  this.currentLabel = "Upload your music!";
+  this.formDisabled = false;
+}
+
 async function convertToImage() {
-  const audioFile = document.getElementById('upload').files[0];
+  if (this.formDisabled) {
+    return;
+  }
+
+  if (this.canvasMounted) {
+    this.canvasMounted = false;
+  }
+
+  const input = document.getElementById('upload');
+  if (input.files.length === 0) {
+    this.currentLabel = 'Please choose an audio file to submit';
+    this.formDisabled = false;
+    return;
+  }
+
+  const audioFile = input.files[0];
   this.formDisabled = true;
-  const res = await getColors('http://localhost:5000/upload', audioFile);
+  this.currentLabel = 'Awaiting response from server...';
+  const url = `${this.baseUrl}/upload`;
+  const res = await getColors(url, audioFile);
   if (!res.ok) {
-    this.currentLabel = 'There was an issue connecting to the server';
-    throw new Error(res.statusText);
+    let message = ''
+    if (res.status === 503) {
+      message = 'Cannot connect to the server';
+    }
+    else {
+      const error = await res.json();
+      message = error.message;
+    }
+
+    this.currentLabel = `${message}: (Error code: ${res.status})`;
+    this.handleError(res.status, this.resetState.bind(this));
+    return;
   }
 
   const pointColors = await res.json();
@@ -83,7 +121,8 @@ export default {
   },
   methods: {
     convertToImage,
-    drawCanvas
+    drawCanvas,
+    resetState
   },
   data() {
     return {
@@ -92,6 +131,20 @@ export default {
       canvasDrawFunction: function() {},
       canvasMounted: false,
       formDisabled: false
+    }
+  },
+  props: {
+    baseUrl: {
+      type: String,
+      required: true
+    },
+    classFromParent: {
+      type: String,
+      required: true
+    },
+    handleError: {
+      type: Function,
+      required: true
     }
   }
 }
