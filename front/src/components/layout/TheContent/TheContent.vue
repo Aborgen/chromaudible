aaf0f49a014e2ae8269ec5896df23d73ca0fba09
Aborgen <alexanderborgen@gmail.com>
2020-03-17 16:03:41 -0700

    Refactor result of @click directives on both audio-upload and image-upload components into individual methods

--- front/src/components/layout/TheContent/TheContent.vue
+++ front/src/components/layout/TheContent/TheContent.vue
@@ -1,12 +1,23 @@
 <template>
 <section :class="classFromParent + ' content-container'">
-  <header>Time to do the thing</header>
   <section class='button-group'>
-    <button @click='currentMode = ModeEnum.AUDIO'>Upload Audio</button>
-    <button @click='currentMode = ModeEnum.IMAGE'>Upload Image</button>
+    <button class='flex-button' @click='setAudioMode'
+            :id='currentMode === ModeEnum.AUDIO && "selected-button"'>Audio</button>
+    <button class='flex-button' @click='setImageMode'
+            :id='currentMode === ModeEnum.IMAGE && "selected-button"'>Image</button>
   </section>
-  <audio-upload v-if=     "currentMode === ModeEnum.AUDIO" />
-  <image-upload v-else-if="currentMode === ModeEnum.IMAGE" />
+  <section class='interactive-container'>
+    <audio-upload v-if             ="currentMode === ModeEnum.AUDIO"
+                  :base-url        ='baseUrl'
+                  :handle-error    ='handleError'
+                  class-from-parent='interactive-item'
+    />
+    <image-upload v-else-if        ="currentMode === ModeEnum.IMAGE"
+                  :base-url        ='baseUrl'
+                  :handle-error    ='handleError'
+                  class-from-parent='interactive-item'
+    />
+  </section>
 </section>
 </template>
 
@@ -15,16 +26,41 @@ import AudioUpload from './internal/AudioUpload/AudioUpload';
 import ImageUpload from './internal/ImageUpload/ImageUpload';
 import ModeEnum from './internal/misc/ModeEnum';
 
+function resetState() {
+  this.allowedRetries = 3;
+}
+
 function setAudioMode() {
-  if (this.currentMode !== ModeEnum.AUDIO) {
-    this.currentMode = ModeEnum.AUDIO;
+  if (this.currentMode === ModeEnum.AUDIO) {
+    return;
   }
+
+  this.resetState();
+  this.currentMode = ModeEnum.AUDIO;
 }
 
 function setImageMode() {
-  if (this.currentMode !== ModeEnum.IMAGE) {
-    this.currentMode = ModeEnum.IMAGE;
+  if (this.currentMode === ModeEnum.IMAGE) {
+    return;
   }
+
+  this.resetState();
+  this.currentMode = ModeEnum.IMAGE;
+}
+
+function handleError(errorCode, func) {
+  if (errorCode === this.lastHTTPCode && this.allowedRetries > 0) {
+    this.allowedRetries--;
+  }
+  else if (this.lastHTTPCode !== 0) {
+    this.allowedRetries++;
+  }
+
+  this.lastHTTPCode = errorCode;
+  const stopTrying = this.allowedRetries <= 0;
+  const message = 'The server is unavailable. Please try again later.';
+  const timeout = stopTrying ? 4000 : 3000;
+  setTimeout(() => func(stopTrying, message), timeout);
 }
 
 export default {
@@ -35,16 +71,21 @@ export default {
   },
   data: function() {
     return {
+      baseUrl: 'http://localhost:5000',
       currentMode: ModeEnum.AUDIO,
-      ModeEnum
+      ModeEnum,
+      lastHTTPCode: 0,
+      allowedRetries: 2
     }
   },
   methods: {
     setAudioMode,
-    setImageMode
+    setImageMode,
+    handleError,
+    resetState
   },
   props: {
-    'classFromParent': {
+    classFromParent: {
       type: String,
       required: true
     }
