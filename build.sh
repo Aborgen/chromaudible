b8f659dd14e3f3058fe94c62b5dc8a5e6fbfdcfe
Aborgen <alexanderborgen@gmail.com>
2020-04-07 13:52:55 -0700

    Add basic shell script that creates a directory that is ready for deployment

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+outDir=./deploy
+
+serverDir=back/
+vueDir=front/
+
+[ -d $outDir ] && rm -rf $outDir && mkdir $outDir
+cp $serverDir/server.py $outDir/main.py
+cp $serverDir/requirements.txt $outDir/requirements.txt
+cp -a $serverDir/src $outDir/src
+echo "Directory $outDir prepared"
+
+npm run --prefix=$vueDir build | tee /dev/tty
