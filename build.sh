31f399d0cab4ae1f0b249bbb21e1720f5bcd3bd3
Aborgen <alexanderborgen@gmail.com>
2020-05-26 16:52:23 -0700

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
+set -e
 
 outDir=./build
 
 serverDir=back/
-vueDir=front/
+clientDir=front/
 
-[ -d $outDir ] && rm -rf $outDir && mkdir $outDir
-cp $serverDir/server.py $outDir/main.py
-cp $serverDir/requirements.txt $outDir/requirements.txt
-cp -a $serverDir/src $outDir/src
-echo "Directory $outDir prepared"
+if [ -d "$outDir" ] 
+then
+  rm -rf "$outDir"
+fi
 
-npm run --prefix=$vueDir build | tee /dev/tty
+mkdir "$outDir"
+printf "Directory $outDir prepared\n"
+cp "$serverDir"/server.py "$outDir"/main.py
+cp -a "$serverDir"/src "$outDir"/src
+printf "Copied python server to $outDir\n\n"
+sleep 0.600
+
+if [ ! -d "$clientDir/node_modules" ]
+then
+  printf "node_modules not found, installing app"
+  sleep 0.600
+  npm install --prefix="$clientDir" | tee /dev/tty
+fi
+
+npm run --prefix="$clientDir" build | tee /dev/tty
